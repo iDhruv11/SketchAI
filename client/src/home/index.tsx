c356c5d78329277a9cb235b62fa9e9118cf8cf2d
iDhruv11 <drvsrm11@gmail.com>
2024-11-18 22:53:45 +0530

    home component test added

---
+++ client/src/home/index.tsx
@@ -0,0 +1,60 @@
+import { ColorSwatch, Group } from '@mantine/core';
+import { Button } from '@/components/ui/button';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import Draggable from 'react-draggable';
+import {SWATCHES} from '@/constants';
+
+export function Home(){
+
+    
+
+    return (
+        <>
+            <div className='grid grid-cols-3 gap-2'>
+                <Button
+                    onClick={() => setReset(true)}
+                    className='z-20 bg-black text-white'
+                    variant='default' 
+                    color='black'
+                >
+                    Reset
+                </Button>
+                <Group className='z-20'>
+                    {SWATCHES.map((swatch) => (
+                        <ColorSwatch key={swatch} color={swatch} onClick={() => setColor(swatch)} />
+                    ))}
+                </Group>
+                <Button
+                    onClick={runRoute}
+                    className='z-20 bg-black text-white'
+                    variant='default'
+                    color='white'
+                >
+                    Run
+                </Button>
+            </div>
+            <canvas
+                ref={canvasRef}
+                id='canvas'
+                className='absolute top-0 left-0 w-full h-full'
+                onMouseDown={startDrawing}
+                onMouseMove={draw}
+                onMouseUp={stopDrawing}
+                onMouseOut={stopDrawing}
+            />
+
+            {latexExpression && latexExpression.map((latex, index) => (
+                <Draggable
+                    key={index}
+                    defaultPosition={latexPosition}
+                    onStop={(e, data) => setLatexPosition({ x: data.x, y: data.y })}
+                >
+                    <div className="absolute p-2 text-white rounded shadow-md">
+                        <div className="latex-content">{latex}</div>
+                    </div>
+                </Draggable>
+            ))}
+        </>
+    );
+}
